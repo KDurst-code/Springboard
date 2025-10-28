94ac76e820bdd0a0502bf03a7b34b66d8b754826
KDurst-code <raeraedurst@gmail.com>
2025-10-27 20:34:06 -0500

    Caesar Cipher exercise files

---
+++ caesar-cipher-exercise/question3.js
@@ -0,0 +1,7 @@
+// Question 3 – Using let and const
+
+let partyLocation = "GARDEN"; // May change
+const shiftValue = 3; // Constant
+let encryptedMessage = ""; // Will change after encryption
+let decryptedMessage = ""; // Will change after decryption
+let isPartySafe = false; // Might change later
