4aa0d35618a3ed189dabb64c7a6ec88e4ce3c7e3
KDurst-code <raeraedurst@gmail.com>
2025-11-06 15:04:50 -0600

    Add new fizzbuzz and letterCount exercises

---
+++ caesar-cipher-exercise/caesar-cipher-exercise/question2.js
@@ -0,0 +1,7 @@
+// Question 2 – Assigning Data Types and Values
+
+let partyLocation = "GARDEN"; // String
+let shiftValue = 3; // Number
+let encryptedMessage = ""; // String
+let decryptedMessage = ""; // String
+let isPartySafe = false; // Boolean
